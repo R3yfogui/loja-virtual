0b40f90eb13142b6676bf44e8952b21c93cb73cb
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-04-02 11:19:50 -0300

    Acho que a aulla 6 foi feita

---
+++ src/layouts/LayoutSmall.vue
@@ -0,0 +1,16 @@
+<script setup>
+import SmallFooter from '../components/templates/small/SmallFooter.vue';
+import SmallHeader from '../components/templates/small/SmallHeader.vue';
+</script>
+
+<template>
+    <header>
+        <small-header />
+    </header>
+    <main>
+        <router-view />
+    </main>
+    <footer>
+        <small-footer />
+    </footer>
+</template>
